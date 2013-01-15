94592e541da9ddefdc167d9801164f1d6395ccd5
Felipe Besson <flpbesson@gmail.com>
2013-01-15 00:21:38 -0200

    checks if item is null

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -12,7 +12,7 @@ class MovieController < ApplicationController
     user_items = UserItem.find(:all, :conditions => ["user_id = ?", @user_id])
     fill_items user_items, @known_items
 
-    rec_items = RecommendedItem.find(:all, :conditions => ["user_id = ?", @user_id], :order => "`order` ASC")
+    rec_items = RecommendedItem.find(:all, :conditions => ["user_id = ?", @user_id], :order => '"order" ASC')
     fill_items rec_items, @recommended_items
 
   end
@@ -20,25 +20,27 @@ class MovieController < ApplicationController
   def fill_items raw_items, complete_items
     raw_items.each do |entry|
       item = Item.find(:first, :conditions => ["item_id = ?", entry.item_id])
-      if item.category.nil?
-        item.category = get_genre item.title, item.extra_info
-        item.save
+      if item != nil then
+        if item.category.nil? then
+          item.category = get_genre item.title, item.extra_info
+          item.save
+        end
+        item.category = item.category.downcase
+        complete_items << item
       end
-      item.category = item.category.downcase
-      complete_items << item
     end
   end
 
-  def get_genre title, year
-    response = RestClient.get "http://www.omdbapi.com", {:params => {:t => title, :y => year}}
-    data = JSON.parse(response)
-    category = data["Genre"]
+    def get_genre title, year
+      response = RestClient.get "http://www.omdbapi.com", {:params => {:t => title, :y => year}}
+      data = JSON.parse(response)
+      category = data["Genre"]
 
-    if category.nil?
-      category = ""
+      if category.nil?
+        category = ""
+      end
+
+      category.downcase
     end
 
-    category.downcase
   end
-
-end
