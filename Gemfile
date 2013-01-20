8c7c5e34d83d124a7501f4ca961e33a269b49cda
Felipe Besson <flpbesson@gmail.com>
2013-01-20 11:58:48 -0200

    testing solr as a recommender engine

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'sqlite3'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -8,7 +8,7 @@ class MovieController < ApplicationController
 
     @known_items = Array.new
     @recommended_items = Array.new
-    @recommended_items_myrrix = Array.new
+    @recommended_items_solr = Array.new
 
     user_items = UserItem.find(:all, :conditions => ["user_id = ?", @user_id])
     fill_items user_items, @known_items
@@ -16,8 +16,8 @@ class MovieController < ApplicationController
     rec_items = RecommendedItem.find(:all, :conditions => ["user_id = ?", @user_id], :order => '"order" ASC')
     fill_items rec_items, @recommended_items
 
-    rec_items_myrrix = get_myrrix_recommendation @user_id
-    fill_items rec_items_myrrix, @recommended_items_myrrix
+    rec_items_solr = get_solr_recommendation @user_id, @known_items
+    fill_items rec_items_solr, @recommended_items_solr
 
   end
 
@@ -25,7 +25,7 @@ class MovieController < ApplicationController
     raw_items.each do |entry|
       item = Item.find(:first, :conditions => ["item_id = ?", entry.item_id])
       if item != nil then
-        if item.category.nil? then
+        if item.category.empty? then
           item.category = get_genre item.title, item.extra_info
           item.save
         end
@@ -47,13 +47,41 @@ class MovieController < ApplicationController
     category.downcase
   end
 
-  def get_myrrix_recommendation user_id
-    items_id = Array.new
+  def get_solr_recommendation user_id, known_items
+    solr_user_url = "http://localhost:8983/solr/user-item/select"
+
+    discover_query = "id:("
+    recommend_query = "user_ids:("
     items = Array.new
 
-    response = RestClient.get "http://localhost/recommend/#{user_id}"
+    known_items.each do |item|
+      discover_query += "#{item.id} OR "
+    end
+
+    discover_query.chomp! " OR "
+    discover_query += ")"
+
+    response = RestClient.get solr_user_url, {:params => {:q => discover_query, :wt => "json", :facet => true, :"facet.field" => "user_ids"}}
     data = JSON.parse(response)
-    data.each_with_index.map {|entry, i| items_id << entry[0] }
+
+    facets = data["facet_counts"]["facet_fields"]["user_ids"]
+
+    facets.each_slice(2) do |slice|
+      if slice[0] != user_id
+        recommend_query += "#{slice[0]}^#{slice[1]} OR "
+      end
+    end
+
+    recommend_query.chomp! " OR "
+    recommend_query += " AND -#{user_id})"
+
+    items_id = Array.new
+    response = RestClient.get solr_user_url, {:params => {:q => recommend_query, :wt => "json"}}
+
+    data = JSON.parse(response)
+    data["response"]["docs"].each do |entry| 
+      items_id << entry["id"]
+    end
 
     items_id.each do |i|
       item = Item.find(:first, :conditions => ["item_id = ?", i])
